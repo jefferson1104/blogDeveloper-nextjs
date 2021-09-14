5a764c876583dd477d17f807bc2b249915e0afee
jefferson1104 <jefferson1104junior@gmail.com>
2021-09-14 00:20:00 -0300

    firebase config, githubauth, contextApi

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import useAuth from '../hooks/useAuth';
+
 export default function Home() {
+  const { user, signin } = useAuth();
+  console.log('USER', user);
+
   return (
     <div>
       <h2>
         Aprenda programação direto ao ponto 100% free.
       </h2>
+      <button onClick={() => signin()}>
+        Entrar com github
+      </button>
     </div>
   )
 }
